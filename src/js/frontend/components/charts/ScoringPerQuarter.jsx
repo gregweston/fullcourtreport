a189690aa9941fc5c4ce4036432f3ca48bbddcd6
Greg Weston <gregweston@users.noreply.github.com>
2017-10-21 11:16:30 -0400

    Dynamically generate labels for ScoringPerQuarter chart so that it will add labels for OT periods if necessary

--- src/js/frontend/components/charts/ScoringPerQuarter.jsx
+++ src/js/frontend/components/charts/ScoringPerQuarter.jsx
@@ -8,10 +8,21 @@ export default class ScoringPerQuarter extends React.Component {
 		this.games = null;
 	}
 
+	generateLabels(periodCount) {
+		const labels = ['Q1', 'Q2', 'Q3', 'Q4'];
+		const overtimePeriodCount = periodCount - 4;
+		if (overtimePeriodCount > 0) {
+			for (let i = 1; i <= overtimePeriodCount; i++) {
+				labels.push('OT' + i.toString());
+			}
+		}
+		return labels;
+	}
+
 	componentDidMount() {
 		const Chartist = require('chartist');
 		new Chartist.Bar('#scoring-per-quarter', {
-			labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+			labels: this.generateLabels(this.props.homeTeamScores.length),
 			series: [
 				{
 					className: 'team ' + this.props.awayTeamAbbreviation,

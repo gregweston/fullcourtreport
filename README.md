ffac29f2cd0c08d82a6582c20bba366ab3a33735
Greg Weston <gregweston@users.noreply.github.com>
2017-11-17 23:41:02 -0500

    Now showing error page if an invalid date is entered in 'date' url

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Fullcourt Report
 
-Fullcourt Report is a web app that generates SVG charts of NBA game data. It uses [XMLStats](https://erikberg.com/api) for the data and [Chartist](https://gionkunz.github.io/chartist-js/) for the charts.
+Fullcourt Report is a web app that generates SVG charts from NBA game data. It uses [XMLStats](https://erikberg.com/api) for the data and [Chartist](https://gionkunz.github.io/chartist-js/) for the charts.
--- src/js/frontend/components/Date.jsx
+++ src/js/frontend/components/Date.jsx
@@ -4,21 +4,27 @@ import ReactDOM from 'react-dom';
 import DateSelect from './DateSelect.jsx';
 import GameList from './GameList.jsx';
 
+const moment = require("moment");
+
 export default class Date extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.games = null;
 		this.state = {
-			dateString: null
+			dateString: null,
+			invalidDate: false
 		};
-
 	}
 
 	setDateString(props) {
 		let dateString;
 		if (props.match.params.date) {
 			dateString = props.match.params.date;
+			let dateCheck = moment(dateString, "YYYYMMDD");
+			if (!dateCheck.isValid()) {
+				this.setState({invalidDate: true});
+			}
 		} else {
 			dateString = this.getDateStringForPreviousDay();
 		}
@@ -37,7 +43,6 @@ export default class Date extends React.Component {
 	}
 
 	setPageTitle(dateString) {
-		const moment = require("moment");
 		const date = moment(dateString, "YYYYMMDD");
 		const formattedDate = date.format("MMM D, YYYY");
 		document.title = `Fullcourt Report - All Games for ${formattedDate}`;
@@ -50,6 +55,9 @@ export default class Date extends React.Component {
 	}
 
 	render() {
+		if (this.state.invalidDate === true) {
+			return <div className="error">Invalid date.</div>
+		}
 		return (
 			<div>
 				<DateSelect date={this.state.dateString} numberOfDatesToDisplay="5" />
